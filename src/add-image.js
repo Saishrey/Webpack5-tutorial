a47a8aa23ab5dc57eff4a37d3ff0b6e6d3978f0a
Shreyas Naik <naiksp16@gmail.com>
2024-06-04 16:29:51 +0530

    import css and sass

--- src/add-image.js
+++ src/add-image.js
@@ -1,8 +1,10 @@
 import Car from './car.png';
+import altText from './alt-text.txt';
 
 function addImage() {
     const img = document.createElement('img');
-    img.alt = 'Porsche 911 GT3 RS';
+    // img.alt = 'Porsche 911 GT3 RS';
+    img.alt = altText;
     img.width = 1366
     img.src = Car;
     const body = document.querySelector('body');
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, './dist')
+        path: path.resolve(__dirname, './dist'),
+        publicPath: 'dist/'
     },
     mode: 'none',
     module: {
@@ -15,8 +16,37 @@ module.exports = {
             // },
             {
                 test: /\.(png|jpg)$/,
-                type: 'asset/resource',
+                // type: 'asset/inline', // images and svgs are embeded in the bundle.js file as base64 representation, reduces http requests, increase file size
+                // type: 'asset/resource', // images and svgs need separate http requests, reduces file size on bundle.js
+                type: 'asset', // webpack will automatically choose between inline and resource types based on the file size(default 8Kb), file size < 8Kb then inline and if > 8Kb then resource
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 1 * 1024 * 1024 // 3Mb
+                    }
+                }
+            },
+            {
+                test: /\.txt/,
+                type: 'asset/source',
+            },
+            // importing css files
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', 
+                    'css-loader'
+                ]
+            },
+            // importing sass files
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
+
         ]
     }
 };
